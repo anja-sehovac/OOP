914a9589372ba522f69e26fe1006ee05375b3650
anja-sehovac <anja.sehovac@stu.ibu.edu.ba>
2023-10-29 14:08:13 +0100

    dependency works after asking professor for help

--- src/main/java/org/example/DbConnect.java
+++ src/main/java/org/example/DbConnect.java
@@ -1,10 +1,13 @@
 package org.example;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DbConnect {
+    public static void main(String[] args) throws SQLException {
+        DbConnect db = new DbConnect();
+
+        db.getTaskById(1);
+    }
+
     private static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/CHANGE";
     private static final String USERNAME = "CHANGE";
     private static final String PASSWORD = "CHANGE";
@@ -20,5 +23,36 @@ public class DbConnect {
             throw new RuntimeException(e);
         }
     }
-}
+    public void getAllTasks() throws SQLException {
+        Statement useStatement = this.connection.createStatement();
+        useStatement.execute("USE lab3;");
+        useStatement.close();
+
+        PreparedStatement statement = this
+                .connection
+                .prepareStatement("SELECT * FROM tasks;");
+        ResultSet rs = statement.executeQuery();
+
+        while (rs.next()) {
+            System.out.println(rs.getString("task_description"));
+            System.out.println(rs.getString("task_status"));
+        }
+    }
+
+    public void getTaskById(int taskId) throws SQLException {
+        Statement useStatement = this.connection.createStatement();
+        useStatement.execute("USE lab3;");
+        useStatement.close();
+
+        PreparedStatement statement = this
+                .connection
+                .prepareStatement("SELECT * FROM tasks WHERE id = ?");
+        statement.setInt(1, taskId);
+        ResultSet rs = statement.executeQuery();
+
+        while (rs.next()) {
+            System.out.println(rs.getString("task_description"));
+            System.out.println(rs.getString("task_status"));
+        }
+    }}
 
